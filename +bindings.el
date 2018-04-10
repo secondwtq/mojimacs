a2e91c91b91b2955ef03b4f3df7a5ce06d48e9dd
secondwtq <lovejay-lovemusic@outlook.com>
2018-04-11 03:47:53 +0800

    Update.

--- +bindings.el
+++ +bindings.el
@@ -8,8 +8,9 @@
 			:desc "？Buffer" 						:n "b" #'counsel-projectile-switch-to-buffer
 			:desc "？Project" 						:n "p" #'counsel-projectile-switch-project
 			:desc "KILL Buffer" 				:n "k" #'projectile-kill-buffers
-			:desc "？Files"							:n "f" #'projectile-recentf
-			:desc "？Recent" 						:n "r" #'counsel-projectile-find-file)
+			:desc "？Files"							:n "f" #'counsel-projectile-find-file
+			:desc "？Recent" 						:n "r" #'projectile-recentf
+      :desc "TREE"                :n "t" #'neotree-find-project-root)
 
       ;     :desc "Browse project"          :n  "." #'+default/browse-project
       ;     :desc "Find other file"         :n  "o" #'projectile-find-other-file
@@ -103,6 +104,97 @@
   ; :desc "Pop scratch buffer"      :n "x" #'doom/open-scratch-buffer
   ; :desc "Bury buffer"             :n "z" #'bury-buffer
   ; :desc "Sudo edit this file"     :n "S" #'doom/sudo-this-file)
+
+    (:desc "file" :prefix "f"
+      ;; Spacemacs
+      :desc "SAVE"         :n "s" #'save-buffer
+      :desc "SAVE ALL"      :n "S" #'evil-write-all
+      :desc "？"            :n "f" #'find-file
+      :desc "？(literally)"  :n "l" #'find-file-literally
+      :desc "TREE"          :n "t" #'neotree-show
+      :desc "？Recent"       :n "r" #'counsel-recentf
+      )
+
+  ;   (spacemacs/set-leader-keys
+  ; "fc" 'spacemacs/copy-file
+  ; "fD" 'spacemacs/delete-current-buffer-file
+  ; "fei" 'spacemacs/find-user-init-file
+  ; "fed" 'spacemacs/find-dotfile
+  ; "feD" 'spacemacs/ediff-dotfile-and-template
+  ; "feR" 'dotspacemacs/sync-configuration-layers
+  ; "fev" 'spacemacs/display-and-copy-version
+  ; "fCd" 'spacemacs/unix2dos
+  ; "fCu" 'spacemacs/dos2unix
+  ; "fg" 'rgrep
+  ; "fE" 'spacemacs/sudo-edit
+  ; "fo" 'spacemacs/open-file-or-directory-in-external-app
+  ; "fR" 'spacemacs/rename-current-buffer-file
+  ; "fvd" 'add-dir-local-variable
+  ; "fvf" 'add-file-local-variable
+  ; "fvp" 'add-file-local-variable-prop-line
+  ; "fy" 'spacemacs/show-and-copy-buffer-filename)
+  ; (:desc "file" :prefix "f"
+  ; :desc "Sudo find file"            :n ">" #'doom/sudo-find-file
+  ; :desc "Find file in project"      :n "/" #'projectile-find-file
+  ; :desc "Find file from here"       :n "?" #'counsel-file-jump
+  ; :desc "Find other file"           :n "a" #'projectile-find-other-file
+  ; :desc "Open project editorconfig" :n "c" #'editorconfig-find-current-editorconfig
+  ; :desc "Find directory"            :n "d" #'dired
+  ; :desc "Find file in emacs.d"      :n "e" #'+default/find-in-emacsd
+  ; :desc "Browse emacs.d"            :n "E" #'+default/browse-emacsd
+  ; :desc "Recent files"              :n "r" #'recentf-open-files
+  ; :desc "Recent project files"      :n "R" #'projectile-recentf
+  ; :desc "Yank filename"             :n "y" #'+default/yank-buffer-filename
+  ; :desc "Find file in private config" :n "p" #'+default/find-in-config
+  ; :desc "Browse private config"       :n "P" #'+default/browse-config)
+
+  (:desc "window" :prefix "w"
+    ;; Spacemacs
+    )
+
+  ; (spacemacs/set-leader-keys
+  ;   "w TAB"  'spacemacs/alternate-window
+  ;   "w2"  'spacemacs/layout-double-columns
+  ;   "w3"  'spacemacs/layout-triple-columns
+  ;   "wb"  'spacemacs/switch-to-minibuffer-window
+  ;   "wd"  'spacemacs/delete-window
+  ;   "wt"  'spacemacs/toggle-current-window-dedication
+  ;   "wf"  'follow-mode
+  ;   "wF"  'make-frame
+  ;   "wH"  'evil-window-move-far-left
+  ;   "w <S-left>"  'evil-window-move-far-left
+  ;   "wh"  'evil-window-left
+  ;   "w <left>"  'evil-window-left
+  ;   "wJ"  'evil-window-move-very-bottom
+  ;   "w <S-down>"  'evil-window-move-very-bottom
+  ;   "wj"  'evil-window-down
+  ;   "w <down>"  'evil-window-down
+  ;   "wK"  'evil-window-move-very-top
+  ;   "w <S-up>"  'evil-window-move-very-top
+  ;   "wk"  'evil-window-up
+  ;   "w <up>"  'evil-window-up
+  ;   "wL"  'evil-window-move-far-right
+  ;   "w <S-right>"  'evil-window-move-far-right
+  ;   "wl"  'evil-window-right
+  ;   "w <right>"  'evil-window-right
+  ;   "wm"  'spacemacs/toggle-maximize-buffer
+  ;   "wc"  'spacemacs/toggle-centered-buffer-mode
+  ;   "wC"  'spacemacs/centered-buffer-mode-full-width
+  ;   "wo"  'other-frame
+  ;   "wr"  'spacemacs/rotate-windows-forward
+  ;   "wR"  'spacemacs/rotate-windows-backward
+  ;   "ws"  'split-window-below
+  ;   "wS"  'split-window-below-and-focus
+  ;   "w-"  'split-window-below
+  ;   "wU"  'winner-redo
+  ;   "wu"  'winner-undo
+  ;   "wv"  'split-window-right
+  ;   "wV"  'split-window-right-and-focus
+  ;   "ww"  'other-window
+  ;   "w/"  'split-window-right
+  ;   "w="  'balance-windows
+  ;   "w+"  'spacemacs/window-layout-toggle
+  ;   "w_"  'spacemacs/maximize-horizontally)
 
     )
   )
--- config.el
+++ config.el
@@ -3,3 +3,9 @@
 (load! +bindings)
 
 (setq doom-theme 'doom-one-light)
+
+;; github.com/hlissner/doom-emacs/tree/master/modules/completion/company
+(require 'company)
+(after! company
+    (setq company-idle-delay 0.2
+        company-minimum-prefix-length 3))
