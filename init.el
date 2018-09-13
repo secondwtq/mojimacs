23c3a9f9f8f849df4f6ec7bbf3d9cbee98f58656
secondwtq <lovejay-lovemusic@outlook.com>
2018-09-14 02:28:28 +0800

    Update.

--- init.el
+++ init.el
@@ -7,7 +7,6 @@
        (evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        (lookup           ; helps you navigate your code and documentation
-        +devdocs         ; ...on devdocs.io online
         +docsets)        ; ...or in Dash docsets locally
        snippets          ; my elves. They type so I don't have to
        spellcheck        ; tasing you for misspelling mispelling
@@ -27,6 +26,7 @@
        ; +fuzzy)          ; enable fuzzy search backend for ivy
 
        :ui
+      ;deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
        doom-modeline     ; a snazzy Atom-inspired mode-line
@@ -51,6 +51,7 @@
        window-select     ; visually switch windows
 
        :editor
+      ;(format +onsave)  ; automated prettiness
        multiple-cursors  ; editing in many places at once
       ;parinfer          ; turn lisp into python, sort of
        rotate-text       ; cycle region at point between text candidates
@@ -67,12 +68,13 @@
        vc                ; version-control and Emacs, sitting in a tree
 
        :tools
+      ;docker
        editorconfig      ; let someone else argue about tabs vs spaces
       ;ein               ; tame Jupyter notebooks with emacs
        gist              ; interacting with github gists
       macos             ; MacOS-specific commands
        make              ; run make tasks from Emacs
-       magit             ;
+       magit             ; a git porcelain for Emacs
        password-store    ; password manager for nerds
        pdf               ; pdf enhancements
       ;prodigy          ; TODO managing external services & code builders
@@ -86,6 +88,7 @@
        (cc +irony + rtags); C/C++/Obj-C madness
        ;clojure           ; java with a lisp
        ;common-lisp       ; if you've seen one lisp, you've seen them all
+       coq               ; proofs-as-programs
        ;crystal           ; ruby at the speed of c
        ;csharp            ; unity, .NET, and mono shenanigans
        data              ; config/data formats
@@ -96,7 +99,8 @@
        ;ess               ; emacs speaks statistics
        ;go                ; the hipster dialect
        (haskell +intero) ; a language that's lazier than I am
-       hy                ; readability of scheme w/ speed of python
+       ;hy                ; readability of scheme w/ speed of python
+       ;idris             ;
        ;(java +meghanada) ; the poster child for carpal tunnel syndrome
        javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;julia             ; a better, faster MATLAB
@@ -124,8 +128,8 @@
        ;ruby              ; 1.step do {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;scala             ; java, but good
-       (sh                ; she sells (ba|z)sh shells on the C xor
-	 +fish
+       (sh                ; she sells (ba|z|fi)sh shells on the C xor
+	 ;+fish
 	 )
       ;solidity          ; do you need a blockchain? No.
        ;swift             ; who asked for emoji variables?
@@ -152,8 +156,8 @@
        ;; literate config in your `doom-private-dir' whenever it changes.
       ;literate
 
-       ;; The default module set reasonable defaults for Emacs. It also provides
-       ;; a Spacemacs-inspired keybinding scheme, a custom yasnippet library,
-       ;; and additional ex commands for evil-mode. Use it as a reference for
-       ;; your own modules.
+       ;; The default module sets reasonable defaults for Emacs. It also
+       ;; provides a Spacemacs-inspired keybinding scheme, a custom yasnippet
+       ;; library, and additional ex commands for evil-mode. Use it as a
+       ;; reference for your own modules.
        (default +bindings +snippets +evil-commands))
