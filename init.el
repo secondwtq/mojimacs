c9bf871a431ff34495836e3654690780944ed920
secondwtq <lovejay-lovemusic@outlook.com>
2021-06-03 09:42:48 +0800

    Update.

--- init.el
+++ init.el
@@ -123,7 +123,8 @@
 
        :lang
        agda              ; types of types of types of types...
-       (cc +lsp); C/C++/Obj-C madness
+       ;;beancount       ; mind the GAAP
+       (cc +lsp)         ; C > C++ == 1
        ;clojure           ; java with a lisp
        ;common-lisp       ; if you've seen one lisp, you've seen them all
        coq               ; proofs-as-programs
@@ -136,6 +137,7 @@
        emacs-lisp        ; drown in parentheses
       ;erlang            ; an elegant language for a more civilized age
        ;ess               ; emacs speaks statistics
+       ;factor
        ;;faust             ; dsp, but you get to keep your soul
        ;;fsharp           ; ML stands for Microsoft's Langauge
        fstar            ; (dependent) types and (monadic) effects and Z3
@@ -150,9 +152,8 @@
        ;julia             ; a better, faster MATLAB
        ;;kotlin           ; a better, slicker Java(Script)
        ;latex             ; writing papers in Emacs has never been so fun
-       lean
-       ;factor
-       ;ledger            ; an accounting system in Emacs
+       lean               ; for folks with too much to prove
+       ;ledger            ; be audit you can be
        ;lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
       ;nim               ; python + lisp at the speed of c
@@ -177,7 +178,7 @@
        ;(ruby +rails)      ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;scala             ; java, but good
-       ;scheme            ; a fully conniving family of lisps
+       ;(scheme +guile)   ; a fully conniving family of lisps
        sh                ; she sells (ba|z|fi)sh shells on the C xor
        ;;sml
       ;solidity          ; do you need a blockchain? No.
@@ -185,8 +186,7 @@
        ;terra             ; Earth and Moon in alignment for performance.
        ;web               ; the tubes
        ;;yaml             ; JSON, but readable
-       ;;vala              ; GObjective-C
-       ;;yaml              ; JSON, but readable
+       ;;zig              ' C, but simpler
 
        :email
        ;;(mu4e +gmail)
@@ -195,6 +195,8 @@
 
        :app
        ;; calendar
+       ;; emms
+       ;; everywhere     ; *leave* Emacs!? You must be joking
       ;irc               ; how neckbeards socialize
       ;(rss +org)        ; emacs as an RSS reader
       ;twitter           ; twitter client https://twitter.com/vnought
